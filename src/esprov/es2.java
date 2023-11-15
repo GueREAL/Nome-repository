32d0b6a03813b9be6e32469c7f458e2b50060bbe
4BI <4BI@10.1.31.16>
2023-11-15 12:48:58 +0100

    Negro a Parigi

--- src/esprov/es2.java
+++ src/esprov/es2.java
@@ -1,9 +1,95 @@
 package esprov;
 
+import java.util.Scanner;
+
 public class es2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner a = new Scanner(System.in);
+		System.out.println("Who was in Paris?");
+		String Str = a.nextLine();
+		if(Str.toLowerCase().equals("nigga")) {
+			for (int i = 0; i<1; i++)
+				System.out.println("Titu titu titu tiWe're gonna skate to one song and one song only\r\n" + 
+						"Ball so hard, motherfuckers wanna fine me\r\n" + 
+						"So I ball so hard, motherfuckers wanna fine me\r\n" + 
+						"But first niggas gotta find me\r\n" + 
+						"What's 50 grand to a motherfucker like me?\r\n" + 
+						"Can you please remind me?\r\n" + 
+						"Ball so hard, this shit crazy\r\n" + 
+						"Y'all don't know that don't shit faze me\r\n" + 
+						"The Nets could go 0 for 82\r\n" + 
+						"And I look at you like this shit gravy\r\n" + 
+						"Ball so hard, this shit weird\r\n" + 
+						"We ain't even 'posed to be here\r\n" + 
+						"Ball so hard, but since we here\r\n" + 
+						"It's only right that we'd be fair\r\n" + 
+						"Psycho, I'm liable to go Michael, take your pick\r\n" + 
+						"Jackson, Tyson, Jordan, Game 6\r\n" + 
+						"Ball so hard, got a broke clock\r\n" + 
+						"Rollies that don't tick-tock\r\n" + 
+						"Audemars that's losing time\r\n" + 
+						"Hidden behind all these big rocks\r\n" + 
+						"B-Ball so hard, I'm shocked too\r\n" + 
+						"I'm supposed to be locked up too\r\n" + 
+						"You escaped what I've escaped\r\n" + 
+						"You'd be in Paris getting fucked up too\r\n" + 
+						"B-Ball so hard, let's get faded\r\n" + 
+						"Le Meurice for like six days\r\n" + 
+						"Gold bottles, scold models\r\n" + 
+						"Spillin' Ace on my sick J's\r\n" + 
+						"Ball so hard, bitch, behave\r\n" + 
+						"Just might let you meet Ye\r\n" + 
+						"Chi-towns, D. Rose\r\n" + 
+						"I'm moving the Nets, BK\r\n" + 
+						"Ball so hard motherfuckers wanna fine me\r\n" + 
+						"That shit cray, that shit cray, that shit cray\r\n" + 
+						"B-Ball so hard motherfuckers wanna fine me\r\n" + 
+						"That shit cray, that shit cray, that shit cray\r\n" + 
+						"She said, \"Ye, can we get married at the mall?\"\r\n" + 
+						"I said, \"Look, you need to crawl 'fore you ball\"\r\n" + 
+						"Come and meet me in the bathroom stall\r\n" + 
+						"And show me why you deserve to have it all\r\n" + 
+						", ain't it Jay?\r\n" + 
+						"(B-Ball so hard) what she order? (What she order?) Fish fillet?\r\n" + 
+						"(B-Ball so hard) your whip so cold (whip so cold), this old thing?\r\n" + 
+						"(Ball so hard) act like you'll never be around motherfuckers like this again\r\n" + 
+						"Bougie girl, grab her hand\r\n" + 
+						"Fuck that bitch, she 'on't wanna dance\r\n" + 
+						"'Scuse my French, but I'm in France, hehe, I'm just sayin'\r\n" + 
+						"Prince William's ain't do it right if you ask me\r\n" + 
+						"'Cause I was him, I would've (married Kate and Ashley)\r\n" + 
+						"What's Gucci, my nigga? What's Louis, my killer?\r\n" + 
+						"What's drugs, my dealer? What's that jacket, Margiela?\r\n" + 
+						"Doctors say I'm the illest\r\n" + 
+						"'Cause I'm suffering from realness\r\n" + 
+						"Got my niggas in Paris and they going gorillas, huh?\r\n" + 
+						"I don't even know what that means\r\n" + 
+						"No one knows what it means, but it's provocative\r\n" + 
+						"No, it's not, it's gross. Gets the people going\r\n" + 
+						"Ball so hard motherfuckers wanna fine me\r\n" + 
+						"B-Ball so hard motherfuckers wanna fine me\r\n" + 
+						"You are now watching the throne\r\n" + 
+						"Don't let me get in my zone\r\n" + 
+						"Don't let me get in my zone\r\n" + 
+						"Don't let me get in my zone\r\n" + 
+						"These other niggas is lyin'\r\n" + 
+						"Actin' like the summer ain't mine\r\n" + 
+						"I got that hot bitch in my home\r\n" + 
+						"You know how many hot bitches I own?\r\n" + 
+						"Don't let me get in my zone\r\n" + 
+						"Don't let me get in my zone\r\n" + 
+						"Don't let me get in my zone\r\n" + 
+						"Don't let me get in my zone\r\n" + 
+						"The stars is in the building\r\n" + 
+						"They hands is to the ceiling\r\n" + 
+						"I know I'm 'bout to kill it\r\n" + 
+						"How you know? I got that feeling\r\n" + 
+						"You are now watching the throne\r\n" + 
+						"Don't let me into my zone\r\n" + 
+						"Don't let me into my zone\r\n" + 
+						"I'm definitely in my zone");
+		}
 
 	}
 
